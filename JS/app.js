de0722f5a9d48afac27333c702ebb84ee3284cf2
JongoSimon <simonjongo@gmail.com>
2025-06-23 14:40:19 -0500

    push

---
+++ JS/app.js
@@ -0,0 +1,11 @@
+let form = document.getElementById('weatherForm');
+let input = document.getElementById('cityInput');
+let resultDiv = document.getElementById('weatherResult');
+
+form.addEventListener('submit') 
+    
+    function(event)){
+        async function fetchData(currentCount) {
+    let url 
+
+}
